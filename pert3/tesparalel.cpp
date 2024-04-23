1efb50d11d651423a26c303eaba72aafd2473b22
djambred <djambred@gmail.com>
2024-04-23 08:51:50 +0700

    pert4

---
+++ pert3/tesparalel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void tambah() {
+    int a = 5;
+    int b = 8;
+    int c = a + b;
+    cout << c << endl;
+}
+int main() {
+    tambah();
+    return 0;
+}
---
+++ pert3/tesparalel.py
@@ -0,0 +1,13 @@
+def angka1():
+    a = 5
+    return a
+
+def angka2():
+    b = 5
+    return b
+
+def tambah():
+    hasil = angka1()+angka2()
+    return hasil
+
+print(tambah())
